00bd2989e7d5a3757ca24cf2916e4bde3b12545f
William Kim <wkim@williams-mbp.home>
2020-05-15 15:48:43 -0700

    show thumbnails

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ const overlay = document.querySelector('.overlay');
 
 for(let i=1; i<=5; i++){
 const newImage = document.createElement('img');
-newImage.setAttribute('src', '../images/pic'+i+'.jpg');
+newImage.setAttribute('src', '/images/pic'+i+'.jpg');
 thumbBar.appendChild(newImage);
 newImage.onclick=function(e){
   displayedImage.src= e.target.src;
